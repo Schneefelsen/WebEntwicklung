04bc6a059591a5259cc3bd74ee739619896fb770
Schneefelsen <sch8tim@gmail.com>
2024-09-04 15:49:28 +0200

    exchanged serviceworker implementation

--- weatherapp_script.js
+++ weatherapp_script.js
@@ -20,6 +20,17 @@ document.getElementById('cityInput').addEventListener('keydown', function(event)
     }
 });
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
+    });
+}
 
 // Function to fetch and display weather data
 async function getWeather() {
