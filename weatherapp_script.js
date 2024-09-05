86cbb785c55952856398e4f8f26781b0fff2c69a
Schneefelsen <sch8tim@gmail.com>
2024-09-05 16:51:34 +0200

    mayor typo corrected

--- weatherapp_script.js
+++ weatherapp_script.js
@@ -22,7 +22,7 @@ document.getElementById('cityInput').addEventListener('keydown', function(event)
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/serviceworker.js')
             .then(registration => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
